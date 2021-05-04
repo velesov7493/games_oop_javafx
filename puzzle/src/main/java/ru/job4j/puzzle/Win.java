a4a3cb0d56cccdbe06aa67956ebccd3ce2a0c87e
velesov7493 <velesov7493@yandex.ru>
2021-05-04 11:22:40 +0000

    2. Сокобан. Тестовое задание. [#75283] Добавлена логика определения победы.

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -1,8 +1,46 @@
 package ru.job4j.puzzle;
 
 public class Win {
+
+    private static boolean monoHorizontal(int[][] board, int row) {
+        boolean result = true;
+        for (int col = 0; col < board[row].length; col++) {
+            if (board[row][col] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    private static boolean monoVertical(int[][] board, int column) {
+        boolean result = true;
+        for (int[] row : board) {
+            if (row[column] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    private static int[] extractDiagonal(int[][] board) {
+        int[] result = new int[board.length];
+        for (int i = 0; i < board.length; i++) {
+            result[i] = board[i][i];
+        }
+        return result;
+    }
+
     public static boolean check(int[][] board) {
-        boolean rsl = true;
-        return rsl;
+        boolean result = false;
+        int[] diagonal = extractDiagonal(board);
+        for (int i = 0; i < diagonal.length; i++) {
+            if (diagonal[i] == 1 && (monoHorizontal(board, i) || monoVertical(board, i))) {
+                result = true;
+                break;
+            }
+        }
+        return result;
     }
 }
